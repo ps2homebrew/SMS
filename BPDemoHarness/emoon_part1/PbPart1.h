996f1223da0a3d1f6f4c0cae64720a236ae8b8b8
emoon <emoon@5c23000f-aefb-0310-8550-8024f1b2be0e>
2004-02-29 19:06:38 +0000

    Added parameters for Camera & Screen matrises
    git-svn-id: http://psp.jim.sh/svn/ps2ware/trunk@68 5c23000f-aefb-0310-8550-8024f1b2be0e

--- BPDemoHarness/emoon_part1/PbPart1.h
+++ BPDemoHarness/emoon_part1/PbPart1.h
@@ -4,7 +4,8 @@
 #include <tamtypes.h>
 #include "PbMatrix.h"
 
-void* PbPart1_DrawObject( PbMatrix* pMatrix, void* pChain );
+void* PbPart1_DrawObject( PbMatrix* pCameraToScreen,PbMatrix* pWorlToCamera,
+                          float* pAngles, void* pChain );
 
 #endif//_PBPART1_H_
 
