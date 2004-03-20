cd391059e1c555a1b39cf08aac1f5c66c18561cb
tyranid <tyranid@5c23000f-aefb-0310-8550-8024f1b2be0e>
2004-03-20 19:51:48 +0000

    Added a demo of FFT stuff
    git-svn-id: http://psp.jim.sh/svn/ps2ware/trunk@133 5c23000f-aefb-0310-8550-8024f1b2be0e

--- BPDemoHarness/c_skel/main.c
+++ BPDemoHarness/c_skel/main.c
@@ -4,6 +4,12 @@
 #include "prim.h"
 #include "../harness.h"
 
+int bars[40];
+#define FALLOFF 2
+int xscale[41] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17,
+		  18, 19, 20, 24, 27, 29, 31, 35, 40, 50, 60, 70, 80, 90, 
+		  100, 120, 140, 160, 180, 240, 300, 350, 450, 511};
+
 u32 start_demo(const demo_init_t *t)
 
 {
@@ -11,6 +17,7 @@ u32 start_demo(const demo_init_t *t)
    u32 colour = 0;
    u32 last_time;
    u32 curr_frame;
+   u16 *fft_data;
 
    init_gs();
    t->printf("Hello World (C)!\n");
@@ -26,8 +33,36 @@ u32 start_demo(const demo_init_t *t)
 
    while(t->frame_count > 0)
    {
+     int loop;
+     int loop2;
+
+     fft_data = t->get_fft();
      clr_scr(colour++); 
-     fill_rect(-10, -20, 320, 112, 0, 0xFFFFFF);
+     for(loop = 0; loop < 40; loop++)
+     { 
+        u16 y;
+
+        y = 0;
+        for(loop2 = xscale[loop]; loop2 < xscale[loop+1]; loop2++)
+        {
+           if(fft_data[loop2] > y)
+             y = fft_data[loop2];
+        }
+ 
+        y >>= 7; 
+
+        if(y > bars[loop])
+        {
+           bars[loop] = y;
+        } 
+        else
+        {
+           bars[loop] -= FALLOFF;
+           if(bars[loop] < 0) bars[loop] = 0;
+        }
+
+        fill_rect(loop * 16, 224 - bars[loop], loop*16 + 16, 224, 0, 0xF000);
+     }
      wait_vsync();
      set_visible_fb(fb);
      fb ^= 1;
