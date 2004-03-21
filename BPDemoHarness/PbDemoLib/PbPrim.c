0220a8667a096f314601465aff7b3b59a7968951
emoon <emoon@5c23000f-aefb-0310-8550-8024f1b2be0e>
2004-03-21 13:49:14 +0000

    Added textured sprite
    git-svn-id: http://psp.jim.sh/svn/ps2ware/trunk@141 5c23000f-aefb-0310-8550-8024f1b2be0e

--- BPDemoHarness/PbDemoLib/PbPrim.c
+++ BPDemoHarness/PbDemoLib/PbPrim.c
@@ -52,6 +52,53 @@ void PbPrimSprite( int x1, int y1, int x2, int y2, int z, int color )
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// void PbPrimSpriteTexture
+///////////////////////////////////////////////////////////////////////////////
+
+void PbPrimSpriteTexture( PbTexture* pTex, int x1, int y1, int u1, int v1, 
+                          int x2, int y2, int u2, int v2, int z, int color  )
+{
+  u64* p_store;
+  u64* p_data;
+
+  x1 += 2048 << 4;
+  y1 += 2048 << 4;
+  x2 += 2048 << 4;
+  y2 += 2048 << 4;
+
+  p_store = p_data = PbSprAlloc( 8*16 );
+  
+  /////////////////////////////////////////////////////////////////////////////
+  // Setup for drawing
+
+  *p_data++ = GIF_TAG( 7, 1, 0, 0, 0, 1 );
+  *p_data++ = GIF_AD;
+
+  *p_data++ = PbTextureGetTex0( pTex );
+  *p_data++ = GS_TEX0_1;
+
+  *p_data++ = GS_SETREG_PRIM( GS_PRIM_PRIM_SPRITE, 0, 1, 0, 0, 0, 1, 0, 0) ;
+  *p_data++ = GS_PRIM;
+
+  *p_data++ = color;
+  *p_data++ = GS_RGBAQ;
+
+  *p_data++ = GS_SETREG_XYZ2( x1, y1, z );
+  *p_data++ = GS_XYZ2;
+
+  *p_data++ = GS_SETREG_UV( u1, v1 );
+  *p_data++ = GS_UV;
+
+  *p_data++ = GS_SETREG_XYZ2( x2, y2, z );
+  *p_data++ = GS_XYZ2;
+
+  *p_data++ = GS_SETREG_UV( u2, v2 );
+  *p_data++ = GS_UV;
+  
+  PbDmaSend02Spr( p_store, 8 );
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // void PbPrimSprite
 ///////////////////////////////////////////////////////////////////////////////
 
--- BPDemoHarness/PbDemoLib/PbPrim.h
+++ BPDemoHarness/PbDemoLib/PbPrim.h
@@ -10,12 +10,17 @@
 #ifndef _PBPRIM_H_
 #define _PBPRIM_H_
 
+#include "PbTexture.h"
+
 ///////////////////////////////////////////////////////////////////////////////
 // Functions
 ///////////////////////////////////////////////////////////////////////////////
 
 void PbPrimSprite( int x1, int y1, int x2, int y2, int z, int color ); 
 void PbPrimSpriteNoZtest( int x1, int y1, int x2, int y2, int z, int color ); 
+
+void PbPrimSpriteTexture( PbTexture* pTex, int x1, int y1, int u1, int v1, 
+                          int x2, int y2, int u2, int v2, int z, int color ); 
 
 
 #endif // _PBPRIM_H_
