df2a186f934dcb1124548034f56673b93ba4ce08
emoon <emoon@5c23000f-aefb-0310-8550-8024f1b2be0e>
2004-03-27 09:54:57 +0000

    Made PbVu1Dump a bit more foolprof (waits for dma and vu1 to finish)
    git-svn-id: http://psp.jim.sh/svn/ps2ware/trunk@186 5c23000f-aefb-0310-8550-8024f1b2be0e

--- BPDemoHarness/PbDemoLib/PbVu1.c
+++ BPDemoHarness/PbDemoLib/PbVu1.c
@@ -9,6 +9,7 @@
 
 #include "PbVu1.h"
 #include "PbFile.h"
+#include "PbDma.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // void PbVu1Dump()
@@ -16,6 +17,8 @@
 
 void PbVu1Dump()
 {
+  PbDmaWait01();
+  PbVu1Wait();
   PbFileWrite( "host:VU1MICROMEM",((void*)0x11008000),1024*16 );
   PbFileWrite( "host:VU1MEM", ((void*)0x1100c000),1024*16 );
 }
