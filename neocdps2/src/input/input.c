0766efcf923490017aec66ef1541e70e3a3737b1
evilo <evilo@5c23000f-aefb-0310-8550-8024f1b2be0e>
2004-09-04 13:28:17 +0000

    Added 2nd controller and analog stick support code cleanup/optimization
    git-svn-id: http://psp.jim.sh/svn/ps2ware/trunk@362 5c23000f-aefb-0310-8550-8024f1b2be0e

--- neocdps2/src/input/input.c
+++ neocdps2/src/input/input.c
@@ -29,18 +29,16 @@
 /*
  * Global var's
  */
-// pad_dma_buf is provided by the user, one buf for each pad
 // contains the pad's current state
-static char padBuf[256] __attribute__((aligned(64)));
+static char padBuf_1[256] __attribute__((aligned(64))) __attribute__ ((section (".bss")));
+static char padBuf_2[256] __attribute__((aligned(64))) __attribute__ ((section (".bss")));
 
 static char actAlign[6];
 static int actuators;
 
-int ret;
-static int port, slot;
-int i;
 static struct padButtonStatus buttons;
 static u32 paddata;
+int pad_nb;
 
 /*--------------------------------------------------------------------------*/
 #define P1UP    0x00000001
@@ -68,9 +66,12 @@ static u32 paddata;
 
 #define SPECIAL 0x01000000
 
+#define PADDLE_1	0
+#define PADDLE_2	1
+
 
 /*--------------------------------------------------------------------------*/
-static uint32 keys   =~0;
+static uint32 keys  =~0;
 /*--------------------------------------------------------------------------*/
 
 
@@ -102,10 +103,35 @@ int waitPadReady(int port, int slot)
 }
 
 /*
+ * padConnected()
+ */
+int padConnected(int port, int slot)
+{
+    int state;
+    int lastState;
+
+    state = padGetState(port, slot);
+    lastState = -1;
+    while((state != PAD_STATE_STABLE) && (state != PAD_STATE_FINDCTP1)) {
+        if (state != lastState)
+        {
+            if (state == PAD_STATE_DISCONN)
+            	return 0;
+        }
+        lastState = state;
+        state=padGetState(port, slot);
+    }
+    // Were the pad ever 'out of sync'?
+    if (lastState != -1) {
+        printf("Pad OK!\n");
+    }
+    return 1;
+}
+
+/*
  * initializePad()
  */
-int
-initializePad(int port, int slot)
+int initializePad(int port, int slot)
 {
 
     int ret;
@@ -162,11 +188,13 @@ initializePad(int port, int slot)
     // When using MMODE_LOCK, user cant change mode with Select button
     padSetMainMode(port, slot, PAD_MMODE_DUALSHOCK, PAD_MMODE_LOCK);
 
+    /*
     waitPadReady(port, slot);
     printf("infoPressMode: %d\n", padInfoPressMode(port, slot));
 
     waitPadReady(port, slot);        
     printf("enterPressMode: %d\n", padEnterPressMode(port, slot));
+    */
 
     waitPadReady(port, slot);
     actuators = padInfoAct(port, slot, -1, 0);
@@ -198,121 +226,211 @@ initializePad(int port, int slot)
 /*--------------------------------------------------------------------------*/
 void input_init(void)
 {
-
+    int ret;
+    
     padInit(0);
 
-    port = 0; // 0 -> Connector 1, 1 -> Connector 2
-    slot = 0; // Always zero if not using multitap
-
+    pad_nb=1;
     printf("PortMax: %d\n", padGetPortMax());
-    printf("SlotMax: %d\n", padGetSlotMax(port));
+    //printf("SlotMax: %d\n", padGetSlotMax(port));
 
-    // only manage joy 1
-    if((ret = padPortOpen(port, slot, padBuf)) == 0)
+
+    // paddle 1
+    printf("Init Paddle 1\n");
+    if((ret = padPortOpen(PADDLE_1, 0, padBuf_1)) != 0)
+    {
+        if(!initializePad(PADDLE_1, 0))
+    	{
+        	printf("pad initalization failed!\n");
+        	SleepThread();
+    	}
+    }
+    else
     {
         printf("padOpenPort failed: %d\n", ret);
         SleepThread();
     }
     
-    if(!initializePad(port, slot))
+    printf("Init Paddle 2\n");
+    // paddle 2
+    
+    if((ret = padPortOpen(PADDLE_2, 0, padBuf_2)) != 0)
     {
-        printf("pad initalization failed!\n");
-        SleepThread();
-    }
+    	if(padConnected(PADDLE_2, 0))
+    	{
+		if(!initializePad(PADDLE_2, 0))
+		{
+	       		printf("pad initalization failed!\n");
+		}
+		else // pad initized
+	  	pad_nb=2;
+	 }
+    } 
+    else
+    {
+		printf("padOpenPort failed: %d\n", ret);
+    }	
 
-
+       
 }
 
 void input_shutdown(void)
 {
- 	;
+ 	padPortClose(PADDLE_1,0);
+ 	padPortClose(PADDLE_2,0);
+ 	padEnd();
 }
 
-inline void joyDown (int which, int button) {
+inline void joySet (int button) {
 	keys &= ~button;
-}
-
-inline void joyUp (int which, int button) {
-        keys |= button;
 }
 
 void processEvents(void)
 {
-	
-    while (padGetState(port, slot) != PAD_STATE_STABLE)
+    // reset key values
+    keys  =~0;
+     	
+    // ------- controller 1 ---------------------------------------
+    while (padGetState(PADDLE_1, 0) != PAD_STATE_STABLE)
     	; // more error check ?
   
-    ret = padRead(port, slot, &buttons); // port, slot, buttons        
-    if (ret != 0)
+    if (padRead(PADDLE_1, 0, &buttons) != 0)
     {
     	paddata = 0xffff ^ ((buttons.btns[0] << 8) | buttons.btns[1]);
-    
-    	if(paddata & PAD_LEFT)
+
+    	if((paddata & PAD_LEFT) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_h < 64)))
     	{
-            joyDown(port, P1LEFT);
-        } else joyUp(port, P1LEFT);
-        
-        if(paddata & PAD_RIGHT)
+            joySet(P1LEFT);
+        }         
+        else if((paddata & PAD_RIGHT) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_h > 192)))
         {
-           joyDown(port, P1RIGHT);
-        } else joyUp(port, P1RIGHT);
+           joySet(P1RIGHT);
+        }
         
-        if(paddata & PAD_UP)
+        if((paddata & PAD_UP) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_v < 64)))
         {
-           joyDown(port, P1UP);
-        } else joyUp(port, P1UP);
-        
-        if(paddata & PAD_DOWN)
+           joySet(P1UP);
+        }        
+        else if ((paddata & PAD_DOWN) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_v > 192)))
         {
-            joyDown(port, P1DOWN);
-        } else joyUp(port, P1DOWN);
+            joySet(P1DOWN);
+        }
         
         if(paddata & PAD_SQUARE)
         {
-            joyDown(port, P1C);
-        } else joyUp(port, P1C);
+            joySet(P1C);
+        }
         
         if(paddata & PAD_CROSS)
         {
-            joyDown(port, P1A);
-        } else joyUp(port, P1A);
+            joySet(P1A);
+        }
         
         if(paddata & PAD_CIRCLE)
         {
-            joyDown(port, P1B);
-        } else joyUp(port, P1B);
+            joySet(P1B);
+        }
         
         if(paddata & PAD_TRIANGLE)
         {
-            joyDown(port, P1D);
-        } else joyUp(port, P1D);
+            joySet(P1D);
+        }
         
         if(paddata & PAD_START)
         {
-           joyDown(port, P1START);
-    	} else joyUp(port, P1START);
+           joySet(P1START);
+    	}
     	
     	if(paddata & PAD_SELECT)
         {
-           joyDown(port, P1SEL);
-    	} else joyUp(port, P1SEL);
+           joySet(P1SEL);
+    	}
     	
-    }
+    	if(paddata & PAD_L1)
+        {
+           AUDIO_PLAYING=!AUDIO_PLAYING;
+	}
+    	
+    } //endif
     
+    // ------- controller 2 ---------------------------------------
+    if (pad_nb ==2) // poll 2nd joystick if detected
+    {
+	while (padGetState(PADDLE_2, 0) != PAD_STATE_STABLE)
+	    	; // more error check ?
+	  
+	if (padRead(PADDLE_2, 0, &buttons) != 0)
+	{
+		paddata = 0xffff ^ ((buttons.btns[0] << 8) | buttons.btns[1]);
+	    
+		if((paddata & PAD_LEFT) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_h < 64)))
+    		{
+            	   joySet(P2LEFT);
+        	}         
+        	else if((paddata & PAD_RIGHT) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_h > 192)))
+        	{
+           	   joySet(P2RIGHT);
+        	}
+        
+        	if((paddata & PAD_UP) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_v < 64)))
+        	{
+           	    joySet(P2UP);
+        	}        
+        	else if ((paddata & PAD_DOWN) || (((buttons.mode >> 4) == 0x07)&&(buttons.ljoy_v > 192)))
+        	{
+            	    joySet(P2DOWN);
+        	}
+	        
+	        if(paddata & PAD_SQUARE)
+	        {
+	            joySet(P2C);
+	        }
+	        
+	        if(paddata & PAD_CROSS)
+	        {
+	            joySet(P2A);
+	        }
+	        
+	        if(paddata & PAD_CIRCLE)
+	        {
+	            joySet(P2B);
+	        }
+	        
+	        if(paddata & PAD_TRIANGLE)
+	        {
+	            joySet(P2D);
+	        }
+	        
+	        if(paddata & PAD_START)
+	        {
+	           joySet(P2START);
+	    	}
+	    	
+	    	if(paddata & PAD_SELECT)
+	        {
+	           joySet(P2SEL);
+	    	}
+	    	
+	    } //endif
+	 } //  //endif joy 2
+	 
 }
         
 /*--------------------------------------------------------------------------*/
-unsigned char read_player1(void) {
+inline unsigned char read_player1(void) 
+{
     return keys&0xff;
 }
 
 /*--------------------------------------------------------------------------*/
-unsigned char read_player2(void) {
+inline unsigned char read_player2(void) 
+{
     return (keys>>8)&0xff;
 }
 
 /*--------------------------------------------------------------------------*/
-unsigned char read_pl12_startsel(void) {
+inline unsigned char read_pl12_startsel(void) 
+{
     return (keys>>16)&0x0f;
 }
 
@@ -322,16 +440,15 @@ void waitforX(void)
    //wait for X key press
    while(1)
    {  
-    	while (padGetState(port, slot) != PAD_STATE_STABLE)
+    	while (padGetState(PADDLE_1, 0) != PAD_STATE_STABLE)
     		; // more error check ?
-  
-    	ret = padRead(port, slot, &buttons); // port, slot, buttons        
-    	if (ret != 0)
+
+    	if (padRead(PADDLE_1, 0, &buttons) != 0)
     	{
 	    	paddata = 0xffff ^ ((buttons.btns[0] << 8) | buttons.btns[1]);
                 if(paddata & PAD_CROSS)
             		return;
     	}
-    } 
-}
+   }
 
+}
--- neocdps2/src/input/input.h
+++ neocdps2/src/input/input.h
@@ -14,9 +14,9 @@ void input_shutdown(void);
 
 void processEvents(void);
 
-unsigned char read_player1(void);
-unsigned char read_player2(void);
-unsigned char read_pl12_startsel(void);
+inline unsigned char read_player1(void);
+inline unsigned char read_player2(void);
+inline unsigned char read_pl12_startsel(void);
 
 /* UI function */
 void waitforX(void);
