fcae69f8cf81f1a9eebb74721ced028ab565a627
doctorxyz <doctorxyz@users.noreply.github.com>
2016-01-19 12:26:05 -0500

    Make it compilable with latest PS2SDK (WIP) -> Remaining AddDmacHandler stuff Adjustments

--- src/SMS_VideoBuffer.c
+++ src/SMS_VideoBuffer.c
@@ -84,7 +84,7 @@ SMS_VideoBuffer* SMS_VideoBufferInit ( int aWidth, int aHeight ) {
  retVal -> m_VFrm[ 2 ].m_pNext = NULL;
  retVal -> m_pFree             = &retVal -> m_VFrm[ 0 ];
 
- retVal -> m_CSCParam.m_HandlerID = AddDmacHandler(3, _csc_handler, retVal -> m_CSCParam);
+ retVal -> m_CSCParam.m_HandlerID = AddDmacHandler(3, _csc_handler, 0);
 
  return retVal;
 
@@ -209,7 +209,7 @@ __asm__(                        // a0 = CSC parameters
  ".set reorder\n\t"
 );
 
-int _csc_handler ( int, void*, void* );
+int _csc_handler ( int );
 __asm__(
  ".set noreorder\n\t"
  ".set nomacro\n\t"
